0c058503c32d891ad8fcd0b9a49fc71c32a0f6bc
Abhisek Dash <abhisekdash6931@gmail.com>
2024-05-25 17:30:58 +0530

    first commit

---
+++ src/main/java/com/example/phone_number_poc/PhoneNumberPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.phone_number_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneNumberPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneNumberPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/phone_number_poc/PhoneNumberPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phone_number_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneNumberPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
